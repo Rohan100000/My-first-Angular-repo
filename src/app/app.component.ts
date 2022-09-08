ca32569a28ba38d67868a249d2c22293b30b04d6
Rohan Shrivastava <rohanshrivastava10000@gmail.com>
2022-09-08 16:23:48 +0530

    Created a simple calculator

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,6 +13,32 @@ export class AppComponent {
 
   listOfNumbers = [1,32,32,55];
   listOfNames = ["Rohan","RAJ","Abhishek","Rinchen"];
+
+  // 2 inputs for calculator
+  num1 = 0;
+  num2 = 0;
+  value = this.num1 + this.num2;
+  // Functions
+  addNumbers = () => {
+    console.log("before:" + this.value);
+    this.value = this.num1 + this.num2;
+    console.log("after:" + this.value);
+  }
+  subtractNumbers = () => {
+    console.log("before:" + this.value);
+    this.value = this.num1 - this.num2;
+    console.log("after:" + this.value);
+  }
+  multiplyNumbers = () => {
+    console.log("before:" + this.value);
+    this.value = this.num1 * this.num2;
+    console.log("after:" + this.value);
+  }
+  divideNumbers = () => {
+    console.log("before:" + this.value);
+    this.value = this.num1 / this.num2;
+    console.log("after:" + this.value);
+  }
   decreaseCount = () => {
     console.log('Decrement Button clicked');
     this.count--;
